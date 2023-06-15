9f020bd97ec308c6f149c44e0a7443b6395d5e52
Lee sang Jik <sangjik9206@gmail.com>
2023-06-15 20:50:48 +0900

    INTERPOLATION
    master class rn

--- App.js
+++ App.js
@@ -1,21 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import {
+  Animated,
+  Dimensions,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.Pressable`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+const { width: SCRENN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function App() {
+  const position = useRef(
+    new Animated.ValueXY({
+      x: -SCRENN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    })
+  ).current;
+  const topLeft = Animated.timing(position, {
+    toValue: {
+      x: -SCRENN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const bottomLeft = Animated.timing(position, {
+    toValue: {
+      x: -SCRENN_WIDTH / 2 + 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const bottomRight = Animated.timing(position, {
+    toValue: {
+      x: SCRENN_WIDTH / 2 - 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const topRight = Animated.timing(position, {
+    toValue: {
+      x: SCRENN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+  const moveUp = () => {
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+    ).start();
+  };
+  const opacity = position.y.interpolate({
+    inputRange: [-250, 0, 250],
+    outputRange: [1, 0.5, 1],
+  });
+
+  const bgColor = position.y.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
+  });
+
+  const border = position.y.interpolate({
+    inputRange: [-250, 250],
+    outputRange: [100, 0],
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <AnimatedBox
+        onPress={moveUp}
+        style={{
+          borderRadius: border,
+          opacity,
+          backgroundColor: bgColor,
+          transform: [...position.getTranslateTransform()],
+        }}
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
